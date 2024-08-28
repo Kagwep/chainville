b19c4bdfc740aa453e9bd72a731265eacd0ea43b
Ted1166 <ogolated00@gmail.com>
2024-08-27 17:55:27 -0700

    contract and hardhat

--- contracts/contracts/ChainVille.sol
+++ contracts/contracts/ChainVille.sol
@@ -8,100 +8,91 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; // added
 contract ChainVille is ERC721, Ownable {
     uint256 private _nextTokenId;
     IERC20 public gameToken; // added
-    uint256 public landPrice; // added
+    uint256 public districtPrice; // added
 
-    struct Land {
+    struct District {
         uint256 x;
         uint256 y;
         string metadata_url;
+        string district_name; // added
         bytes32 stateHash;
         uint256 lastUpdate;
     }
 
-    mapping(uint256 => Land) private _lands;
+    mapping(uint256 => District) private _districts;
     mapping(uint256 => mapping(uint256 => uint256)) private _coordinateToTokenId;
-    mapping(address => uint256[]) private _ownedLands;
-    mapping(uint256 => uint256) private _landVersion; // added
-    mapping(uint256 => mapping(string => uint256)) private _landResources; // added
+    mapping(address => uint256[]) private _ownedDistricts;
+    mapping(uint256 => uint256) private _districtVersion; // added
+    mapping(uint256 => mapping(string => uint256)) private _districtResources; // added
 
-
-    event LandAcquired(address indexed owner, uint256 indexed tokenId, uint256 x, uint256 y, string metadata_url);
-    event LandStateUpdated(uint256 indexed tokenId, string metadata_url, bytes32 stateHash, uint256 timestamp);
+    event DistrictAcquired(address indexed owner, uint256 indexed tokenId, uint256 x, uint256 y, string metadata_url, string district_name);
+    event DistrictStateUpdated(uint256 indexed tokenId, string metadata_url, bytes32 stateHash, uint256 timestamp);
     event InfrastructureBuilt(uint256 indexed tokenId, string infrastructureType); // Added
+    event Withdrawal(address indexed owner, uint256 amount);
 
-    constructor(address initialOwner, address tokenAddress, uint256 _landPrice) ERC721("ChainVille", "CV") Ownable(initialOwner) {
+    constructor(address initialOwner, address tokenAddress, uint256 _districtPrice) ERC721("ChainVille", "CV") Ownable(initialOwner) {
         _nextTokenId = 0;
         gameToken = IERC20(tokenAddress); // added
-        landPrice = _landPrice; // added
+        districtPrice = _districtPrice; // added
     }
 
     // Added
     function buildInfrastructure(uint256 tokenId, string memory infrastructureType) public {
-        require(_isAuthorized(msg.sender, tokenId), "Not owner nore approved");
+        require(_isAuthorized(msg.sender, tokenId), "Not owner nor approved");
 
         // Assuming each infrastructure requires 100 units of a certain resource
-        require(_landResources[tokenId]["basic_resource"] >=100, "Insufficient Resources");
+        require(_districtResources[tokenId]["basic_resource"] >=100, "Insufficient Resources");
 
-        _landResources[tokenId]["basic_resource"] -= 100;
+        _districtResources[tokenId]["basic_resource"] -= 100;
         
         emit InfrastructureBuilt(tokenId, infrastructureType);
     }
 
-    function acquireLand(uint256 x, uint256 y, string memory metadata_url) public {
-        require(_coordinateToTokenId[x][y] == 0, "Land already acquired");
-        require(gameToken.transferFrom(msg.sender, address(this), landPrice), "Payment Failed"); // added
+    function acquireDistrict(uint256 x, uint256 y, string memory metadata_url, string memory district_name) public {
+        require(_coordinateToTokenId[x][y] == 0, "District already acquired");
+        require(gameToken.transferFrom(msg.sender, address(this), districtPrice), "Payment Failed"); // added
         
         uint256 newTokenId = _nextTokenId++;
         _safeMint(msg.sender, newTokenId);
         
-        _lands[newTokenId] = Land(x, y, metadata_url, bytes32(0), 0);
+        _districts[newTokenId] = District(x, y, metadata_url, district_name, bytes32(0), 0);
         _coordinateToTokenId[x][y] = newTokenId;
-        _ownedLands[msg.sender].push(newTokenId);
+        _ownedDistricts[msg.sender].push(newTokenId);
         
-        emit LandAcquired(msg.sender, newTokenId, x, y, metadata_url);
+        emit DistrictAcquired(msg.sender, newTokenId, x, y, metadata_url, district_name);
     } 
 
-    // editted
-    function updateLandState(uint256 tokenId, string memory metadata_url, bytes32 stateHash) public {
+    // edited
+    function updateDistrictState(uint256 tokenId, string memory metadata_url, bytes32 stateHash) public {
         require(_isAuthorized(msg.sender, tokenId), "Not owner nor approved");
-        Land storage land = _lands[tokenId];
-        _landVersion[tokenId]++;
+        District storage district = _districts[tokenId];
+        _districtVersion[tokenId]++;
 
-        land.metadata_url = metadata_url;
-        land.stateHash = stateHash;
-        land.lastUpdate = block.timestamp;
+        district.metadata_url = metadata_url;
+        district.stateHash = stateHash;
+        district.lastUpdate = block.timestamp;
         
-        emit LandStateUpdated(tokenId, metadata_url, stateHash, block.timestamp);
+        emit DistrictStateUpdated(tokenId, metadata_url, stateHash, block.timestamp);
     }
-
-    // function updateLandState(uint256 tokenId, string memory metadata_url, bytes32 stateHash) public {
-    //     require(_isAuthorized(msg.sender, tokenId), "Not owner nor approved");
-    //     Land storage land = _lands[tokenId];
-    //     land.metadata_url = metadata_url;
-    //     land.stateHash = stateHash;
-    //     land.lastUpdate = block.timestamp;
-        
-    //     emit LandStateUpdated(tokenId, metadata_url, stateHash, block.timestamp);
-    // }
 
     // Added
-    // Allows querying the current version of the land
-    function getLandVersion(uint256 tokenId) public view returns (uint256) {
-        return _landVersion[tokenId];
+    // Allows querying the current version of the district
+    function getDistrictVersion(uint256 tokenId) public view returns (uint256) {
+        return _districtVersion[tokenId];
     }
 
-    function getLandDetails(uint256 tokenId) public view returns (uint256 x, uint256 y, string memory metadata_url, bytes32 stateHash, uint256 lastUpdate) {
-        require(_ownerOf(tokenId) != address(0), "Land does not exist");
-        Land memory land = _lands[tokenId];
-        return (land.x, land.y, land.metadata_url, land.stateHash, land.lastUpdate);
+    function getDistrictDetails(uint256 tokenId) public view returns (uint256 x, uint256 y, string memory metadata_url, string memory district_name, bytes32 stateHash, uint256 lastUpdate) {
+        require(_ownerOf(tokenId) != address(0), "District does not exist");
+        District memory district = _districts[tokenId];
+        return (district.x, district.y, district.metadata_url, district.district_name, district.stateHash, district.lastUpdate);
     }
 
-    function isLandAcquired(uint256 x, uint256 y) public view returns (bool) {
+    function isDistrictAcquired(uint256 x, uint256 y) public view returns (bool) {
         return _coordinateToTokenId[x][y] != 0;
     }
 
     function getTokenIdByCoordinates(uint256 x, uint256 y) public view returns (uint256) {
-        require(_coordinateToTokenId[x][y] != 0, "Land not acquired");
+        require(_coordinateToTokenId[x][y] != 0, "District not acquired");
         return _coordinateToTokenId[x][y];
     }
 
@@ -112,22 +103,22 @@ contract ChainVille is ERC721, Ownable {
             _removeTokenFromOwnerList(from, tokenId);
         }
         if (to != address(0)) {
-            _ownedLands[to].push(tokenId);
+            _ownedDistricts[to].push(tokenId);
         }
         
         return from;
     }
 
-    function getLandsByOwner(address owner) public view returns (uint256[] memory) {
-        return _ownedLands[owner];
+    function getDistrictsByOwner(address owner) public view returns (uint256[] memory) {
+        return _ownedDistricts[owner];
     }
 
     function _removeTokenFromOwnerList(address owner, uint256 tokenId) private {
-        uint256[] storage ownerLands = _ownedLands[owner];
-        for (uint i = 0; i < ownerLands.length; i++) {
-            if (ownerLands[i] == tokenId) {
-                ownerLands[i] = ownerLands[ownerLands.length - 1];
-                ownerLands.pop();
+        uint256[] storage ownerDistricts = _ownedDistricts[owner];
+        for (uint i = 0; i < ownerDistricts.length; i++) {
+            if (ownerDistricts[i] == tokenId) {
+                ownerDistricts[i] = ownerDistricts[ownerDistricts.length - 1];
+                ownerDistricts.pop();
                 break;
             }
         }
@@ -140,16 +131,53 @@ contract ChainVille is ERC721, Ownable {
     }
 
     // added
-    // owner to update parameters like land prices
-    function setLandPrice(uint256 newPrice) public onlyOwner {
-        landPrice = newPrice;
+    // owner to update parameters like district prices
+    function setDistrictPrice(uint256 newPrice) public onlyOwner {
+        districtPrice = newPrice;
     }
 
     // added
-    // allows owner to change the ERC20token used in the game
+    // allows owner to change the ERC20 token used in the game
     function setTokenAddress(address newTokenAddress) public onlyOwner {
         gameToken = IERC20(newTokenAddress);
     }
 
+    // Allows players to merge two or more districts into a larger, more powerful district
+    function mergeDistricts(uint256[] memory tokenIds, string memory newMetadataUrl, string memory newDistrictName) public {
+        require(tokenIds.length > 1, "At least two districts required for merging");
 
+        uint256 newTokenId = _nextTokenId++;
+        uint256 x = 0;
+        uint256 y = 0;
+
+        // Calculate new coordinates (could be based on the average of all district cordinates)
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            require(_isAuthorized(msg.sender, tokenIds[i]), "Not owned nor approved");
+            District memory district = _districts[tokenIds[i]];
+            x += district.x;
+            y += district.y;
+
+            // Burn old districts
+            _burn(tokenIds[i]);
+        }
+
+        x /= tokenIds.length;
+        y /= tokenIds.length;
+
+        _districts[newTokenId] = District(x, y, newMetadataUrl, newDistrictName, bytes32(0), block.timestamp);
+        _safeMint(msg.sender, newTokenId);
+        _ownedDistricts[msg.sender].push(newTokenId);
+
+        emit DistrictAcquired(msg.sender, newTokenId, x, y, newMetadataUrl, newDistrictName);
+    }
+
+    // added withdraw 
+    function withdraw(uint256 amount, bool withdrawToken) public onlyOwner {
+        if (withdrawToken) {
+            require(gameToken.transfer(msg.sender, amount), "Token withdrawal failed");
+        } else {
+            payable(owner()).transfer(amount);
+        }
+        emit Withdrawal(msg.sender, amount);
+    }
 }
